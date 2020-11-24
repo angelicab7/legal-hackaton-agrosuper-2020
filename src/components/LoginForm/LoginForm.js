bd22033a876fbf25ffefc479d23519392455e5c1
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-23 23:10:50 -0500

    login styles and components

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import { useForm } from 'react-hook-form';
+
+const LoginForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography
+          variant="h3"
+          component="h1"
+          align="center"
+          className="margin-b-one"
+        >
+          Bienvenido
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            name="username"
+            label="Usuario"
+            variant="outlined"
+            inputRef={register}
+            required
+            fullWidth
+            className="margin-b-one"
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Contraseña"
+            variant="outlined"
+            inputRef={register}
+            required
+            fullWidth
+            className="margin-b-one"
+          />
+          <Box display="flex" justifyContent="center">
+            <Button variant="contained" color="secondary" type="submit">
+              Iniciar sesión
+            </Button>
+          </Box>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LoginForm;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LoginForm from '../components/LoginForm/LoginForm';
+import logo from '../assets/images/logo-header.png';
 
 const Home = () => (
-  <Container>
-    <Typography variant="h1">Hello World</Typography>
-    <Button variant="contained" color="secondary">
-      Hello World
-    </Button>
-  </Container>
+  <div className="home-container">
+    <Container>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item lg={6}>
+          <Box display="flex" justifyContent="center">
+            <img src={logo} alt="AgroSuper Logo" />
+          </Box>
+          <LoginForm />
+        </Grid>
+      </Grid>
+    </Container>
+  </div>
 );
 
 export default Home;
