f073f2d53a49382abb7ebc06525605ba0208f9bb
Samantha <xsamynox@gmail.com>
2020-11-25 14:52:39 -0300

    select and input in the same line

--- src/components/RegulationsForm/RegulationsForm.js
+++ src/components/RegulationsForm/RegulationsForm.js
@@ -14,8 +14,8 @@ import {
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
-    margin: theme.spacing(1),
-    minWidth: 60,
+    margin: theme.spacing(2),
+    minWidth: 100,
   },
 }));
 
@@ -50,126 +50,160 @@ const RegulationsForm = () => {
 
   return (
     <Container>
+      <h5>Cumplimiento de Normativa:</h5>
       <form onSubmit={sendForm}>
-        <h6>Exigencias medioambientales</h6>
-        <FormControl className={classes.formControl}>
-          <InputLabel>Si/No</InputLabel>
-          <Select
-            labelId="select-id"
-            id="yes-ex"
-            value={environmental}
-            onChange={handleChange}
+        <InputLabel>Exigencias Medioambientales</InputLabel>
+        <div className="reg-select">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Si/No</InputLabel>
+            <Select
+              labelId="select-id"
+              id="yes-ex"
+              value={environmental}
+              onChange={handleChange}
+              className="select-sino"
+              size="small"
+              required
+            >
+              <MenuItem value="Si">Si</MenuItem>
+              <MenuItem value="No">No</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            name=""
+            type="text"
+            label="¿Cuáles?"
+            variant="outlined"
+            className="margin-b-one"
+            size="small"
             required
-          >
-            <MenuItem value="Si">Si</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          name=""
-          type="text"
-          label="¿Cuáles?"
-          variant="outlined"
-          className="margin-b-one"
-          required
-        />
+            fullWidth
+          />
+        </div>
 
-        <h6>Certificaciones requeridas</h6>
-        <FormControl className={classes.formControl}>
-          <InputLabel>Si/No</InputLabel>
-          <Select
-            labelId="select-id"
-            id="yes-cr"
-            value={certifications}
-            onChange={handleChange1}
+        <InputLabel>Certificaciones requeridas</InputLabel>
+        <div className="reg-select">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Si/No</InputLabel>
+            <Select
+              labelId="select-id"
+              id="yes-cr"
+              value={certifications}
+              onChange={handleChange1}
+              size="small"
+              required
+            >
+              <MenuItem value="Si">Si</MenuItem>
+              <MenuItem value="No">No</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            name="input-certifications"
+            type="text"
+            label="¿Cuáles?"
+            variant="outlined"
+            className="margin-b-one"
             required
-          >
-            <MenuItem value="Si">Si</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          name="input-certifications"
-          type="text"
-          label="¿Cuáles?"
-          variant="outlined"
-          className="margin-b-one"
-          required
-        />
+            size="small"
+            fullWidth
+          />
+        </div>
 
-        <h6>Licencia de conducir</h6>
-        <FormControl className={classes.formControl}>
-          <InputLabel>Si/No</InputLabel>
-          <Select
-            labelId="select-id"
-            id="yes-lc"
-            value={license}
-            onChange={handleChange2}
+        <InputLabel>Licencia de conducir</InputLabel>
+        <div className="reg-select">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Si/No</InputLabel>
+            <Select
+              labelId="select-id"
+              id="yes-lc"
+              value={license}
+              onChange={handleChange2}
+              size="small"
+              required
+            >
+              <MenuItem value="Si">Si</MenuItem>
+              <MenuItem value="No">No</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            name="input-license"
+            type="text"
+            label="¿Cuáles?"
+            variant="outlined"
+            className="margin-b-one"
+            size="small"
             required
-          >
-            <MenuItem value="Si">Si</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          name="input-license"
-          type="text"
-          label="¿Cuáles?"
-          variant="outlined"
-          className="margin-b-one"
-          required
-        />
+            fullWidth
+          />
+        </div>
 
-        <h6>Cursos especiales</h6>
-        <FormControl className={classes.formControl}>
-          <InputLabel>Si/No</InputLabel>
-          <Select
-            labelId="select-id"
-            id="yes-ce"
-            value={courses}
-            onChange={handleChange3}
+        <InputLabel>Cursos especiales</InputLabel>
+        <div className="reg-select">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Si/No</InputLabel>
+            <Select
+              labelId="select-id"
+              id="yes-ce"
+              value={courses}
+              onChange={handleChange3}
+              size="small"
+              required
+            >
+              <MenuItem value="Si">Si</MenuItem>
+              <MenuItem value="No">No</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            name="input-courses"
+            type="text"
+            label="¿Cuáles?"
+            variant="outlined"
+            className="margin-b-one"
+            size="small"
             required
-          >
-            <MenuItem value="Si">Si</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          name="input-courses"
-          type="text"
-          label="¿Cuáles?"
-          variant="outlined"
-          className="margin-b-one"
-          required
-        />
+            fullWidth
+          />
+        </div>
 
-        <h6>Otros</h6>
-        <FormControl className={classes.formControl}>
-          <InputLabel>Si/No</InputLabel>
-          <Select
-            labelId="select-id"
-            id="yes-o"
-            value={others}
-            onChange={handleChange4}
+        <InputLabel>Otros</InputLabel>
+        <div className="reg-select">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Si/No</InputLabel>
+            <Select
+              labelId="select-id"
+              id="yes-o"
+              value={others}
+              onChange={handleChange4}
+              size="small"
+              required
+            >
+              <MenuItem value="Si">Si</MenuItem>
+              <MenuItem value="No">No</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            name="input-others"
+            type="text"
+            label="¿Cuáles?"
+            variant="outlined"
+            className="margin-b-one"
+            size="small"
             required
-          >
-            <MenuItem value="Si">Si</MenuItem>
-            <MenuItem value="No">No</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          name="input-others"
-          type="text"
-          label="¿Cuáles?"
-          variant="outlined"
-          className="margin-b-one"
-          required
-        />
-        <Box display="flex" justifyContent="center" className="w100">
-          <Button variant="contained" color="primary" type="submit">
-            Siguiente
-          </Button>
-        </Box>
+            fullWidth
+          />
+        </div>
+        <div className='ctn-button'>
+          <Box display="flex" justifyContent="center" className="w100">
+            <Button variant="contained" color="default" type="submit">
+              Volver
+            </Button>
+          </Box>
+          <Box display="flex" justifyContent="center" className="w100">
+            <Button variant="contained" color="primary" type="submit">
+              Siguiente
+            </Button>
+          </Box>
+        </div>
       </form>
     </Container>
   );
