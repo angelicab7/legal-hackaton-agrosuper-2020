688fa01da05bc6e0aecc1b61ee6d91081622989e
CatalinaJL <catalina.p.jl@gmail.com>
2020-11-24 22:07:59 -0300

    First form structure  with MaterialUI

---
+++ src/components/ContractorsForm/ContractorsForm.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment, useState } from 'react';
+import { FormControl } from '@material-ui/core/FormControl';
+import { InputLabel } from '@material-ui/core/InputLabel';
+
+
+const ContractorsForm = () => {
+
+    return
+    <Container>
+        <FormControl>
+            <InputLabel htmlFor=''> Razón Social </InputLabel>
+        </FormControl>
+    </Container>
+
+};
+
+
+export default ContractorsForm;
