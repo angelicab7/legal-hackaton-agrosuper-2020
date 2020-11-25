5e8f65716e92c43fd07c2c72a25c279d52a40fb8
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-25 08:35:41 -0500

    microsoft login

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from './styles/material-ui-theme';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
+import theme from './styles/material-ui-theme';
 import './styles/main.scss';
 import App from './app';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*
 const firebaseConfig = {
-  ...Firebase Config
+  apiKey: 'AIzaSyAOG9oYqBtjsBRhsUS2foVa6LnpNsN1z4o',
+  authDomain: 'agrosuper-legal-hackaton-2020.firebaseapp.com',
+  databaseURL: 'https://agrosuper-legal-hackaton-2020.firebaseio.com',
+  projectId: 'agrosuper-legal-hackaton-2020',
+  storageBucket: 'agrosuper-legal-hackaton-2020.appspot.com',
+  messagingSenderId: '198287142003',
+  appId: '1:198287142003:web:62a79a9f9f0bf0697f8e06',
+  measurementId: 'G-RTDD3L0NRR',
 };
-*/
 
 // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const app = (
   <BrowserRouter>
