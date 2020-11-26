9eda4c14dcd3acbe157ce18bc613544c58b9f91d
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-25 20:48:40 -0500

    change login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,9 +3,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import TextField from '@material-ui/core/TextField';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Box from '@material-ui/core/Box';
-import { useForm } from 'react-hook-form';
 import firebase from 'firebase/app';
 
 import microsoftSignInLogo from '../../assets/images/ms-symbollockup_signin_dark.svg';
@@ -20,12 +19,6 @@ provider.setCustomParameters({
 });
 
 const LoginForm = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
   const onMicrosoftLogin = () => {
     firebase
       .auth()
@@ -45,44 +38,24 @@ const LoginForm = () => {
           variant="h3"
           component="h1"
           align="center"
-          className="margin-b-one"
+          className="margin-b-three"
         >
           Bienvenido
         </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            name="username"
-            label="Usuario"
-            variant="outlined"
-            inputRef={register}
-            required
-            fullWidth
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <AccountCircleIcon
+            style={{ fontSize: 100 }}
             className="margin-b-one"
           />
-          <TextField
-            name="password"
-            type="password"
-            label="Contraseña"
-            variant="outlined"
-            inputRef={register}
-            required
-            fullWidth
-            className="margin-b-one"
-          />
-          <Box display="flex" justifyContent="center">
-            <Button variant="contained" color="primary" type="submit">
-              Iniciar sesión
-            </Button>
-          </Box>
-          <Box display="flex" justifyContent="center">
-            <Button type="button" onClick={onMicrosoftLogin}>
-              <img
-                src={microsoftSignInLogo}
-                alt="Iniciar sesión con Microsoft"
-              />
-            </Button>
-          </Box>
-        </form>
+          <Button type="button" onClick={onMicrosoftLogin}>
+            <img src={microsoftSignInLogo} alt="Iniciar sesión con Microsoft" />
+          </Button>
+        </Box>
       </CardContent>
     </Card>
   );
