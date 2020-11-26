324e0ada2d0838aa5aff9f72d26b38438c3ffe34
CatalinaJL <catalina.p.jl@gmail.com>
2020-11-26 20:25:06 -0300

    npm sendEmail-js install, and function send email to client works

--- src/containers/ChoiceForm.js
+++ src/containers/ChoiceForm.js
@@ -3,6 +3,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import firebase from 'firebase/app';
+import emailjs from 'emailjs-com';
 import ContractorsForm from '../components/ContractorsForm/ContractorsForm';
 import ServicesForm from '../components/ServicesForm/ServicesForm';
 // Components
@@ -12,43 +13,45 @@ import PlazoTarifa from '../components/ChoiceForm/PlazoTarifa';
 import RegulationsForm from '../components/RegulationsForm/RegulationsForm';
 import Finalize from '../components/FinalizeForm/Finalize';
 import CenteredBox from '../components/CenteredBox';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 import ProgressBar from '../components/ProgressBar';
-
 
 const ChoicesForm = () => {
   const [answers, setAnswers] = useState({});
   const [sendData, setSendData] = useState(false);
   const { push } = useHistory();
-  const [testData, setTest] = useState({ bgcolor: '#002089', completed: 0 })
+  const [testData, setTest] = useState({ bgcolor: '#002089', completed: 0 });
+  const frmContrato = { id_solicitud: '001 ' };
+  const [contract, setContract] = useState(frmContrato);
+  const [showMessage, setShowMessage] = useState(false);
 
   const onNextDependencySelector = (data) => {
     setAnswers((previousAnswers) => ({ ...previousAnswers, ...data }));
-    setTest({ bgcolor: '#002089', completed: 20 })
+    setTest({ bgcolor: '#002089', completed: 20 });
     push('/preguntas/contratistas');
   };
 
   const onNextContractorsForm = (data) => {
     setAnswers((previousAnswers) => ({ ...previousAnswers, ...data }));
-    setTest({ bgcolor: '#002089', completed: 40 })
+    setTest({ bgcolor: '#002089', completed: 40 });
     push('/preguntas/servicios');
   };
 
   const onNextServicesForm = (data) => {
     setAnswers((previousAnswers) => ({ ...previousAnswers, ...data }));
-    setTest({ bgcolor: '#002089', completed: 60 })
+    setTest({ bgcolor: '#002089', completed: 60 });
     push('/preguntas/serviciosyfaltas');
   };
 
   const onNextServicesFaults = (data) => {
     setAnswers((previousAnswers) => ({ ...previousAnswers, ...data }));
-    setTest({ bgcolor: '#002089', completed: 80 })
+    setTest({ bgcolor: '#002089', completed: 80 });
     push('/preguntas/plazoytarifas');
   };
 
   const onNextPlazoTarifa = (data) => {
     setAnswers((previousAnswers) => ({ ...previousAnswers, ...data }));
-    setTest({ bgcolor: '#002089', completed: 100 })
+    setTest({ bgcolor: '#002089', completed: 100 });
     push('/preguntas/otras');
   };
 
@@ -78,10 +81,33 @@ const ChoicesForm = () => {
         ...answers,
       });
   };
+  const sendEmail = () => {
+    emailjs
+      .send(
+        'default_service',
+        'template_r1wow8m',
+        {
+          id_solicitud: contract.id_solicitud,
+        },
+
+        'user_MNiKynjQh7oIXGtmHlZiO',
+      )
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          setContract(frmContrato);
+          setShowMessage(true);
+        },
+        (err) => {
+          console.log('FAILED...', err);
+        },
+      );
+  };
 
   useEffect(() => {
     if (sendData) {
       sendDataToFirebase();
+      sendEmail();
     }
   }, [answers, sendData]);
 
@@ -110,41 +136,63 @@ const ChoicesForm = () => {
             <Route
               path="/preguntas/contratistas"
               render={(props) => (
-                <ContractorsForm {...props} onNext={onNextContractorsForm} handler={setTest}/>
+                <ContractorsForm
+                  {...props}
+                  onNext={onNextContractorsForm}
+                  handler={setTest}
+                />
               )}
             />
             <Route
               path="/preguntas/servicios"
               render={(props) => (
-                <ServicesForm {...props} onNext={onNextServicesForm} handler={setTest}/>
+                <ServicesForm
+                  {...props}
+                  onNext={onNextServicesForm}
+                  handler={setTest}
+                />
               )}
             />
             <Route
               path="/preguntas/serviciosyfaltas"
               render={(props) => (
-                <ServicesFaults {...props} onNext={onNextServicesFaults} handler={setTest}/>
+                <ServicesFaults
+                  {...props}
+                  onNext={onNextServicesFaults}
+                  handler={setTest}
+                />
               )}
             />
             <Route
               path="/preguntas/plazoytarifas"
               render={(props) => (
-                <PlazoTarifa {...props} onNext={onNextPlazoTarifa} handler={setTest}/>
+                <PlazoTarifa
+                  {...props}
+                  onNext={onNextPlazoTarifa}
+                  handler={setTest}
+                />
               )}
             />
             <Route
               path="/preguntas/otras"
               render={(props) => (
-                <RegulationsForm {...props} onNext={onNextOtras} handler={setTest}/>
+                <RegulationsForm
+                  {...props}
+                  onNext={onNextOtras}
+                  handler={setTest}
+                />
               )}
             />
             <Route
               path="/preguntas/finalize"
-              render={(props) => <Finalize {...props} onNext={onNextOtras} handler={setTest}/>}
+              render={(props) => (
+                <Finalize {...props} onNext={onNextOtras} handler={setTest} />
+              )}
             />
           </Switch>
         </CardContent>
       </Card>
-      <Footer/>
+      <Footer />
     </CenteredBox>
   );
 };
