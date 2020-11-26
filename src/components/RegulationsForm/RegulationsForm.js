72b3307444a44a4c1fe5fd4c2e72350c0bc30f04
Samantha <xsamynox@gmail.com>
2020-11-25 22:39:13 -0300

    add modal final form

--- src/components/RegulationsForm/RegulationsForm.js
+++ src/components/RegulationsForm/RegulationsForm.js
@@ -10,10 +10,33 @@ import {
   MenuItem,
   Box,
   Button,
-  // Modal,
+  Modal,
 } from '@material-ui/core';
 
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
 const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
   formControl: {
     margin: theme.spacing(2),
     minWidth: 100,
@@ -21,9 +44,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const RegulationsForm = () => {
-  const { valuesForm, handleSubmit } = useForm();
-
   const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const { register, handleSubmit } = useForm();
 
   const [environmental, setEnvironmental] = React.useState('');
   const [certifications, setCertifications] = React.useState('');
@@ -47,46 +70,52 @@ const RegulationsForm = () => {
     setOthers(event.target.value);
   };
 
-  const sendForm = (event, data) => {
+  const sendForm = (data) => {
     // event.preventDefault();
-    console.log(data, event);
+    console.log(data);
+    handleOpen();
   };
 
-  // const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  // const bodyModal = (
-  //   <div>
-  //     <h2 id="simple-modal-title">Text in a modal</h2>
-  //     <p id="simple-modal-description">
-  //       Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-  //     </p>
-  //   </div>
-  // );
+  const bodyModal = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Tu solicitud se ha enviado correctamente</h2>
+      <p id="simple-modal-description">
+        Tu solicitud de contrato <span>Nº001</span> se envio correctamente. Para
+        mayor detalle favor revisa en tu bandeja de correo electrónico.
+      </p>
+    </div>
+  );
 
   return (
     <Container>
       <h2>Cumplimiento de Normativa:</h2>
-      <form onSubmit={handleSubmit(sendForm)}>
+      <form
+        onSubmit={handleSubmit(sendForm)}
+        ref={register({ required: true })}
+      >
         <InputLabel>Exigencias Medioambientales</InputLabel>
         <div className="reg-select">
           <FormControl className={classes.formControl}>
             <InputLabel>Si/No</InputLabel>
             <Select
+              name="environmentalSelect"
               labelId="select-id"
               id="yes-ex"
               value={environmental}
               onChange={handleChange}
               className="select-sino"
               size="small"
-              inputRef={valuesForm}
+              inputRef={register({ required: true })}
               required
             >
               <MenuItem value="Si">Si</MenuItem>
@@ -94,13 +123,13 @@ const RegulationsForm = () => {
             </Select>
           </FormControl>
           <TextField
-            name=""
+            name="environmentalInput"
             type="text"
             label="¿Cuáles?"
             variant="outlined"
             className="margin-b-one"
             size="small"
-            inputRef={valuesForm}
+            inputRef={register({ required: true })}
             required
             fullWidth
           />
@@ -111,12 +140,13 @@ const RegulationsForm = () => {
           <FormControl className={classes.formControl}>
             <InputLabel>Si/No</InputLabel>
             <Select
+              name="certificationsSelect"
               labelId="select-id"
               id="yes-cr"
               value={certifications}
               onChange={handleChange1}
               size="small"
-              inputRef={valuesForm}
+              inputRef={register({ required: true })}
               required
             >
               <MenuItem value="Si">Si</MenuItem>
@@ -124,14 +154,14 @@ const RegulationsForm = () => {
             </Select>
           </FormControl>
           <TextField
-            name="input-certifications"
+            name="certificationsInput"
             type="text"
             label="¿Cuáles?"
             variant="outlined"
             className="margin-b-one"
             required
             size="small"
-            inputRef={valuesForm}
+            inputRef={register({ required: true })}
             fullWidth
           />
         </div>
@@ -141,12 +171,13 @@ const RegulationsForm = () => {
           <FormControl className={classes.formControl}>
             <InputLabel>Si/No</InputLabel>
             <Select
+              name="licenseSelect"
               labelId="select-id"
               id="yes-lc"
               value={license}
               onChange={handleChange2}
               size="small"
-              inputRef={valuesForm}
+              inputRef={register({ required: true })}
               required
             >
               <MenuItem value="Si">Si</MenuItem>
@@ -154,13 +185,13 @@ const RegulationsForm = () => {
             </Select>
           </FormControl>
           <TextField
-            name="input-license"
+            name="licenseInput"
             type="text"
             label="¿Cuáles?"
             variant="outlined"
             className="margin-b-one"
             size="small"
-            inputRef={valuesForm}
+            inputRef={register({ required: true })}
             required
             fullWidth
           />
@@ -171,12 +202,13 @@ const RegulationsForm = () => {
           <FormControl className={classes.formControl}>
             <InputLabel>Si/No</InputLabel>
             <Select
+              name="coursesSelect"
               labelId="select-id"
               id="yes-ce"
               value={courses}
               onChange={handleChange3}
               size="small"
-              inputRef={valuesForm}
+              inputRef={register({ required: true })}
               required
             >
               <MenuItem value="Si">Si</MenuItem>
@@ -184,13 +216,13 @@ const RegulationsForm = () => {
             </Select>
           </FormControl>
           <TextField
-            name="input-courses"
+            name="coursesInput"
             type="text"
             label="¿Cuáles?"
             variant="outlined"
             className="margin-b-one"
             size="small"
-            inputRef={valuesForm}
+            inputRef={register({ required: true })}
             required
             fullWidth
           />
@@ -201,12 +233,13 @@ const RegulationsForm = () => {
           <FormControl className={classes.formControl}>
             <InputLabel>Si/No</InputLabel>
             <Select
+              name="othersSelect"
               labelId="select-id"
               id="yes-o"
               value={others}
               onChange={handleChange4}
               size="small"
-              inputRef={valuesForm}
+              inputRef={register({ required: true })}
               required
             >
               <MenuItem value="Si">Si</MenuItem>
@@ -214,41 +247,36 @@ const RegulationsForm = () => {
             </Select>
           </FormControl>
           <TextField
-            name="input-others"
+            name="othersSelect"
             type="text"
             label="¿Cuáles?"
             variant="outlined"
             className="margin-b-one"
             size="small"
-            inputRef={valuesForm}
+            inputRef={register({ required: true })}
             required
             fullWidth
           />
         </div>
         <div className="ctn-button">
           <Box display="flex" justifyContent="center" className="w100">
-            <Button variant="contained" color="secondary" type="submit">
+            <Button variant="contained" color="secondary" type="button">
               Volver
             </Button>
           </Box>
           <Box display="flex" justifyContent="center" className="w100">
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-            // onClick={handleOpen}
-            >
-              Siguiente
+            <Button variant="contained" color="primary" type="submit">
+              Finalizar
             </Button>
           </Box>
-          {/* <Modal
+          <Modal
             open={open}
             onClose={handleClose}
             aria-labelledby="simple-modal-title"
             aria-describedby="simple-modal-description"
           >
             {bodyModal}
-          </Modal> */}
+          </Modal>
         </div>
       </form>
     </Container>
