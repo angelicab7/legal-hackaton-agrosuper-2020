856decd1f48d469129654af5ee34f6729d7bee30
Samantha <xsamynox@gmail.com>
2020-11-26 14:19:12 -0300

    component finalize, message final

--- src/components/RegulationsForm/RegulationsForm.js
+++ src/components/RegulationsForm/RegulationsForm.js
@@ -16,25 +16,6 @@ import {
 const RegulationsForm = ({ onNext }) => {
   const { register, handleSubmit, control } = useForm();
 
-  // const [open, setOpen] = React.useState(false);
-
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
-
-  // const bodyModal = (
-  //   <div>
-  //     <h2 id="simple-modal-title">Text in a modal</h2>
-  //     <p id="simple-modal-description">
-  //       Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-  //     </p>
-  //   </div>
-  // );
-
   return (
     <>
       <Typography
@@ -221,14 +202,6 @@ const RegulationsForm = ({ onNext }) => {
             Siguiente
           </Button>
         </Box>
-        {/* <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-          >
-            {bodyModal}
-          </Modal> */}
       </form>
     </>
   );
