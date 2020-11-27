c967f0edeace84bbd264ab1dda97b09369307140
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-26 23:15:53 -0500

    Some improvements and fixes

--- src/containers/Lawyers.js
+++ src/containers/Lawyers.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 // Components
 import Box from '@material-ui/core/Box';
-import logo from '../assets/images/logo-header.svg';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import TableInfo from '../components/Table/TableInfo';
 
 const SelectUserView = () => (
-  <Container className={`d-flex grow-1`}>
+  <Container className="d-flex grow-1">
     <Box
       display="flex"
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      className="centered-box-container2"
+      className="centered-box-container2 margin-y-two"
     >
-      <TableInfo/>
+      <Typography
+        variant="h3"
+        component="h1"
+        align="center"
+        className="margin-b-one"
+      >
+        Solicitudes
+      </Typography>
+      <TableInfo />
     </Box>
   </Container>
 );
