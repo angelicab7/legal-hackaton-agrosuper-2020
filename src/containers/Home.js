c141089eba6ac562f566642fb7e8758b160eae23
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-26 21:35:35 -0500

    Fix footer styles

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm/LoginForm';
 import CenteredBox from '../components/CenteredBox';
-import Footer from '../components/Footer';
 
 const Home = () => (
   <CenteredBox>
     <LoginForm />
-    <Footer />
   </CenteredBox>
 );
 
